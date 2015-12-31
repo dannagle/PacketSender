4a194483a15b8dafface946626d78cac449c7610
Dan Nagle <dan@dannagle.com>
2015-12-30 18:51:58 -0600

    check valid ipv4 address

--- src/packet.cpp
+++ src/packet.cpp
@@ -598,10 +598,10 @@ QByteArray Packet::HEXtoByteArray(QString thehex)
 
 QString Packet::removeIPv6Mapping(QHostAddress ipv6)
 {
-    bool ok;
-    quint32 ipv4 = ipv6.toIPv4Address(&ok);
+    quint32 ipv4 = ipv6.toIPv4Address();
 
-    if(ok) {
+    //valid address will have a result greater than 0
+    if(ipv4 > 0) {
         QHostAddress new_ipv4(ipv4);
         return new_ipv4.toString();
     } else {
