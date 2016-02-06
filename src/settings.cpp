95c3ba1fcc71248e94b82999971cbffbb14ce7d3
Dan Nagle <dan@dannagle.com>
2016-02-05 21:26:27 -0600

    got UI for smart replies working

--- src/settings.cpp
+++ src/settings.cpp
@@ -33,6 +33,42 @@ Settings::Settings(QWidget *parent) :
     setWindowTitle("Packet Sender Settings");
     setWindowIcon(mIcon);
 
+
+    //smart responses...
+
+#define RESPONSEIF(NUM) ui->responseIfEdit##NUM->setText(settings.value("responseIfEdit" #NUM,"").toString())
+
+    RESPONSEIF(1);
+    RESPONSEIF(2);
+    RESPONSEIF(3);
+    RESPONSEIF(4);
+    RESPONSEIF(5);
+
+#define RESPONSEREPLY(NUM) ui->responseReplyEdit##NUM->setText(settings.value("responseReplyEdit"#NUM,"").toString())
+
+    RESPONSEREPLY(1);
+    RESPONSEREPLY(2);
+    RESPONSEREPLY(3);
+    RESPONSEREPLY(4);
+    RESPONSEREPLY(5);
+
+
+#define RESPONSEENABLEMACRO(NUM) ui->responseEnableCheck##NUM->setChecked(settings.value("responseEnableCheck"#NUM, false).toBool())
+
+    RESPONSEENABLEMACRO(1);
+    RESPONSEENABLEMACRO(2);
+    RESPONSEENABLEMACRO(3);
+    RESPONSEENABLEMACRO(4);
+    RESPONSEENABLEMACRO(5);
+
+#define ENCODEMACRO(NUM) ui->responseEncodingBox##NUM->setCurrentIndex(ui->responseEncodingBox##NUM->findText(settings.value("responseEncodingBox"#NUM,"Mixed ASCII").toString()))
+
+    ENCODEMACRO(1);
+    ENCODEMACRO(2);
+    ENCODEMACRO(3);
+    ENCODEMACRO(4);
+    ENCODEMACRO(5);
+
     ui->udpServerPortEdit->setText(settings.value("udpPort","55056").toString());
     ui->tcpServerPortEdit->setText(settings.value("tcpPort","55056").toString());
 
@@ -95,6 +131,7 @@ Settings::Settings(QWidget *parent) :
     setStoredTableHeaders();
     loadTableHeaders();
 
+    ui->copyUnformattedCheck->setFocus();
     QDEBUG() << "Settings Loaded";
 
 }
@@ -167,6 +204,45 @@ void Settings::on_buttonBox_accepted()
         packetSavedHeaderNow.append(lwTraffic->item(i)->text());
     }
     settings.setValue("packetTableHeaders", packetSavedHeaderNow);
+
+
+    //smart responses...
+
+
+#define RESPONSEIFSAVE(NUM) settings.setValue("responseIfEdit" #NUM, ui->responseIfEdit##NUM->text());
+
+    RESPONSEIFSAVE(1);
+    RESPONSEIFSAVE(2);
+    RESPONSEIFSAVE(3);
+    RESPONSEIFSAVE(4);
+    RESPONSEIFSAVE(5);
+
+#define RESPONSEREPLYSAVE(NUM) settings.setValue("responseReplyEdit" #NUM, ui->responseReplyEdit##NUM->text());
+
+    RESPONSEREPLYSAVE(1);
+    RESPONSEREPLYSAVE(2);
+    RESPONSEREPLYSAVE(3);
+    RESPONSEREPLYSAVE(4);
+    RESPONSEREPLYSAVE(5);
+
+
+#define RESPONSEENABLEMACROSAVE(NUM) settings.setValue("responseEnableCheck" #NUM, ui->responseEnableCheck##NUM->isChecked());
+
+    RESPONSEENABLEMACROSAVE(1);
+    RESPONSEENABLEMACROSAVE(2);
+    RESPONSEENABLEMACROSAVE(3);
+    RESPONSEENABLEMACROSAVE(4);
+    RESPONSEENABLEMACROSAVE(5);
+
+
+#define ENCODEMACROSAVE(NUM) settings.setValue("responseEncodingBox" #NUM, ui->responseEncodingBox##NUM->currentText());
+
+    ENCODEMACROSAVE(1);
+    ENCODEMACROSAVE(2);
+    ENCODEMACROSAVE(3);
+    ENCODEMACROSAVE(4);
+    ENCODEMACROSAVE(5);
+
 
 
 }
