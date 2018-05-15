193a1bc77f9ddc8c11db265136f33315f47cc6ed
Dan Nagle <dan@dannagle.com>
2018-05-14 20:16:39 -0500

    Improvements to CI/CD

--- ci_cd/windows/win-prebuild.bat
+++ ci_cd/windows/win-prebuild.bat
@@ -1,2 +1,4 @@
-copy C:\Users\Dan\Desktop\prebuild-windows.sh .
-C:\cygwin64\bin\sh.exe -xe prebuild-windows.sh
+set BUILD_VERSION=5.7.%BUILD_NUMBER%
+echo %BUILD_VERSION% > buildversion.txt
+REM copy /Y C:\Users\Dan\Desktop\prebuild-windows.php ci_cd\windows\win-prebuild.php
+C:\cygwin64\bin\php.exe ci_cd/windows/win-prebuild.php
