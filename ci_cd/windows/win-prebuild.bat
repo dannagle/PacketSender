3d7b7cbc4bb92686565b11e356373bbedafd41e8
Dan Nagle <dan@dannagle.com>
2018-09-15 15:12:57 -0500

    adding multicast. version bump. #74

--- ci_cd/windows/win-prebuild.bat
+++ ci_cd/windows/win-prebuild.bat
@@ -1,4 +1,4 @@
-set BUILD_VERSION=5.7.%BUILD_NUMBER%
+set BUILD_VERSION=5.8.%BUILD_NUMBER%
 echo %BUILD_VERSION% > buildversion.txt
 REM copy /Y C:\Users\Dan\Desktop\prebuild-windows.php ci_cd\windows\win-prebuild.php
 C:\cygwin64\bin\php.exe ci_cd/windows/win-prebuild.php
