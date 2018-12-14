817cc8a10395a47544656baf0b04570745b468ec
Dan Nagle <dan@dannagle.com>
2018-12-14 16:20:44 -0600

    Update CI/CD Windows to 64-bit libraries

--- ci_cd/windows/win-deploy.bat
+++ ci_cd/windows/win-deploy.bat
@@ -1,7 +1,10 @@
 echo Building Installers
 set /p BUILD_VERSION=<buildversion.txt
 set "BUILD_VERSION=%BUILD_VERSION: =%"
-cd C:\Users\Dan\github\packetsenderinstaller
+cd C:\Users\Dan\github\packetsenderinstaller\installer
+move *.* ../archive
+cd ..
+
 "C:\Program Files\7-Zip\7z.exe" a PacketSenderPortable_v%BUILD_VERSION%.zip PacketSenderPortable
 move PacketSenderPortable_v%BUILD_VERSION%.zip installer
 "C:\Program Files (x86)\Inno Setup 5\ISCC.exe" packetsender64bit.iss
