62d1b19e40a3dd3f7bf20ab70d9652e03acf7bf2
Dan Nagle <dan@dannagle.com>
2019-03-23 17:27:51 -0500

    version bump

--- ci_cd/windows/win-prebuild.bat
+++ ci_cd/windows/win-prebuild.bat
@@ -1,4 +1,4 @@
-set BUILD_VERSION=6.0.%BUILD_NUMBER%
+set BUILD_VERSION=6.1.%BUILD_NUMBER%
 echo %BUILD_VERSION% > buildversion.txt
 REM copy /Y C:\Users\Dan\Desktop\prebuild-windows.php ci_cd\windows\win-prebuild.php
 C:\cygwin64\bin\php.exe ci_cd/windows/win-prebuild.php
