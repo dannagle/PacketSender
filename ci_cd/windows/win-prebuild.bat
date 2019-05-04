8aef94fec58c3d2dea475941d33a0d0e62973eac
Dan Nagle <dan@dannagle.com>
2019-05-04 15:51:48 -0500

    fix build number

--- ci_cd/windows/win-prebuild.bat
+++ ci_cd/windows/win-prebuild.bat
@@ -1,4 +1,4 @@
-set BUILD_VERSION=1
+set BUILD_NUMBER=1
 set BUILD_VERSION=6.2.%BUILD_NUMBER%
 echo %BUILD_VERSION% > buildversion.txt
 REM copy /Y C:\Users\danie\Desktop\prebuild-windows.php ci_cd\windows\win-prebuild.php
