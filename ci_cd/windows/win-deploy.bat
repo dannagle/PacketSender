e7c669991cd730ba31517053df1881c3f4f49014
Dan Nagle <dan@dannagle.com>
2020-06-21 19:06:47 -0500

    migrate qt to 5.15.0

--- ci_cd/windows/win-deploy.bat
+++ ci_cd/windows/win-deploy.bat
@@ -7,7 +7,7 @@ cd ..
 
 "C:\Program Files\7-Zip\7z.exe" a PacketSenderPortable_v%BUILD_VERSION%.zip PacketSenderPortable
 move PacketSenderPortable_v%BUILD_VERSION%.zip installer
-"C:\Program Files (x86)\Inno Setup 5\ISCC.exe" packetsender64bit.iss
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" packetsender64bit.iss
 cd installer
 copy /Y C:\Users\danie\Desktop\code_sign_exe_com.bat .
 call code_sign_exe_com.bat
