5181d25aff6c8c138dffcec13f288a3bb1569fca
Dan Nagle <dan@dannagle.com>
2021-03-29 18:44:23 -0500

    version bump 7.2.1

--- ci_cd/windows/win-build.bat
+++ ci_cd/windows/win-build.bat
@@ -1,12 +1,12 @@
 echo Making exe
-C:\Qt\5.15.0\mingw81_64\bin\qmake.exe -o Makefile src/PacketSender.pro -spec win32-g++
+C:\Qt\5.15.2\mingw81_64\bin\qmake.exe -o Makefile src/PacketSender.pro -spec win32-g++
 C:\Qt\Tools\mingw810_64\bin\mingw32-make.exe -f Makefile.Release
 echo Signing exe
 cd release
 copy /Y C:\Users\danie\Desktop\code_sign_exe_com.bat .
 call code_sign_exe_com.bat
 echo Copying signed exe
-copy /Y packetsender.exe D:\github\naglecode-installers\packetsenderinstaller\q5_15_0_mingw64\packetsender.exe
+copy /Y packetsender.exe D:\github\naglecode-installers\packetsenderinstaller\q5_15_2_mingw64\packetsender.exe
 copy /Y packetsender.exe D:\github\naglecode-installers\packetsenderinstaller\PacketSenderPortable\packetsender.exe
 cd ..
 
@@ -14,7 +14,7 @@ echo Cleaning build
 rd /s /q release
 
 echo Making command line exe
-C:\Qt\5.15.0\mingw81_64\bin\qmake.exe -o Makefile src/PacketSenderCLI.pro -spec win32-g++
+C:\Qt\5.15.2\mingw81_64\bin\qmake.exe -o Makefile src/PacketSenderCLI.pro -spec win32-g++
 C:\Qt\Tools\mingw810_64\bin\mingw32-make.exe -f Makefile.Release
 
 echo Signing command line exe
@@ -22,6 +22,6 @@ cd release
 copy /Y C:\Users\danie\Desktop\code_sign_exe_com.bat .
 call code_sign_exe_com.bat
 echo Copying command line exe
-copy /Y packetsendercli.exe D:\github\naglecode-installers\packetsenderinstaller\q5_15_0_mingw64\packetsender.com
+copy /Y packetsendercli.exe D:\github\naglecode-installers\packetsenderinstaller\q5_15_2_mingw64\packetsender.com
 copy /Y packetsendercli.exe D:\github\naglecode-installers\packetsenderinstaller\PacketSenderPortable\packetsender.com
 cd ..
--- ci_cd/windows/win-prebuild.bat
+++ ci_cd/windows/win-prebuild.bat
@@ -6,5 +6,5 @@ wsl php ci_cd/windows/win-prebuild.php
 GOTO End1
 
 :No1
-  ECHO Need build version param (such as 6.2.1)
+  ECHO Need build version param (such as 7.2.1)
 :End1
