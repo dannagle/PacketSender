f8e4c8c7b1cb4a00c4467b463d19cf97c700aefb
Dan Nagle <dan@dannagle.com>
2023-01-05 10:45:18 -0600

    Fix spacing issue in version output

--- src/main.cpp
+++ src/main.cpp
@@ -179,14 +179,14 @@ int main(int argc, char *argv[])
         QTextStream out(stdout);
 
         QCoreApplication::setApplicationName("Packet Sender");
-        QString versionBuilder = QString("version ") + SW_VERSION;
+        QString versionBuilder = QString("Version ") + SW_VERSION;
         if (QSslSocket::supportsSsl()) {
             versionBuilder.append(" / SSL:");
             versionBuilder.append(QSslSocket::sslLibraryBuildVersionString());
         }
 
         #ifdef GIT_CURRENT_SHA1
-            versionBuilder.append("/ Commit: " + QString(GIT_CURRENT_SHA1));
+            versionBuilder.append(" / Commit: " + QString(GIT_CURRENT_SHA1));
         #endif
 
         QCoreApplication::setApplicationVersion(versionBuilder);
