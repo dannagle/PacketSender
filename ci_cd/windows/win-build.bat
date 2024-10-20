f11d05214c0961c0091d4abf52fb66fd2903e52a
Dan Nagle <dan@dannagle.com>
2024-10-19 21:11:27 -0500

    migrate to qt6.8.0

--- ci_cd/windows/win-build.bat
+++ ci_cd/windows/win-build.bat
@@ -1,13 +1,13 @@
 echo Making exe
-SET PATH=%PATH%;C:\Qt\Tools\mingw1120_64\bin;C:\Qt\6.5.3\mingw_64\bin
-C:\Qt\6.5.3\mingw_64\bin\qmake.exe -o Makefile src/PacketSender.pro -spec win32-g++
-C:\Qt\Tools\mingw1120_64\bin\mingw32-make.exe -f Makefile.Release -j8
+SET PATH=%PATH%;C:\Qt\Tools\mingw1310_64\bin;C:\Qt\6.8.0\mingw_64\bin
+C:\Qt\6.8.0\mingw_64\bin\qmake.exe -o Makefile src/PacketSender.pro -spec win32-g++
+C:\Qt\Tools\mingw1310_64\bin\mingw32-make.exe -f Makefile.Release -j8
 echo Signing exe
 cd release
 copy /Y C:\Users\danie\Desktop\code_sign_exe_com.bat .
 call code_sign_exe_com.bat
 echo Copying signed exe
-copy /Y packetsender.exe C:\Users\danie\github\naglecode-installers\packetsenderinstaller\qt6_5_2_mingw64\packetsender.exe
+copy /Y packetsender.exe C:\Users\danie\github\naglecode-installers\packetsenderinstaller\qt6_8_0_mingw64\packetsender.exe
 copy /Y packetsender.exe C:\Users\danie\github\naglecode-installers\packetsenderinstaller\PacketSenderPortable\packetsender.exe
 cd ..
 
@@ -15,14 +15,14 @@ echo Cleaning build
 rd /s /q release
 
 echo Making command line exe
-C:\Qt\6.5.3\mingw_64\bin\qmake.exe -o Makefile src/PacketSenderCLI.pro -spec win32-g++
-C:\Qt\Tools\mingw1120_64\bin\mingw32-make.exe -f Makefile.Release -j8
+C:\Qt\6.8.0\mingw_64\bin\qmake.exe -o Makefile src/PacketSenderCLI.pro -spec win32-g++
+C:\Qt\Tools\mingw1310_64\bin\mingw32-make.exe -f Makefile.Release -j8
 
 echo Signing command line exe
 cd release
 copy /Y C:\Users\danie\Desktop\code_sign_exe_com.bat .
 call code_sign_exe_com.bat
 echo Copying command line exe
-copy /Y packetsendercli.exe C:\Users\danie\github\naglecode-installers\packetsenderinstaller\qt6_5_2_mingw64\packetsender.com
+copy /Y packetsendercli.exe C:\Users\danie\github\naglecode-installers\packetsenderinstaller\qt6_8_0_mingw64\packetsender.com
 copy /Y packetsendercli.exe C:\Users\danie\github\naglecode-installers\packetsenderinstaller\PacketSenderPortable\packetsender.com
 cd ..
