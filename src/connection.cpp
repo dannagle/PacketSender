e0c119ab37fe266a1a26cc233d7e28e037ed4e88
Tomas Gallucci <professortom@gmail.com>
2026-03-05 13:34:00 -0600

    add `Connection` class to project

---
+++ src/connection.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tomas Gallucci on 3/5/26.
+//
+
+#include "connection.h"
---
+++ src/connection.h
@@ -0,0 +1,16 @@
+//
+// Created by Tomas Gallucci on 3/5/26.
+//
+
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+
+
+class Connection {
+
+};
+
+
+
+#endif //CONNECTION_H
