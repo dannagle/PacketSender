0dd1fd1aa58f65a04d0111241d9108b197788121
Tomas Gallucci <professortom@gmail.com>
2026-03-06 15:30:57 -0600

    add unit tests for new TcpThread constructor

--- src/tests/unit/tcpthreadtests.h
+++ src/tests/unit/tcpthreadtests.h
@@ -5,12 +5,16 @@
 #ifndef TCPTHREADTESTS_H
 #define TCPTHREADTESTS_H
 
+#include <QObject>
 
 
-class TcpThread_tests {
-
+class TcpThreadTests : public QObject
+{
+    Q_OBJECT
+private slots:
+    void testIncomingConstructorBasic();
+    void testIncomingConstructorWithSecureFlag();
 };
-
 
 
 #endif //TCPTHREADTESTS_H
