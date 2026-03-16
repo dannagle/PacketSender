a80043058e3d97bb158bc8ce74ee42ee623bff1d
Tomas Gallucci <professortom@gmail.com>
2026-03-16 18:34:47 -0500

    add unit tests for `getIPConnectionProtocol()`

--- src/tests/unit/tcpthreadtests.h
+++ src/tests/unit/tcpthreadtests.h
@@ -14,6 +14,17 @@ class TcpThreadTests : public QObject
 private slots:
     void testIncomingConstructorBasic();
     void testIncomingConstructorWithSecureFlag();
+
+    // testDetermineIpMode() tests
+    void testGetIPConnectionProtocol_bothIPv4_returnsIPv4();
+    void testGetIPConnectionProtocol_bothIPv6_returnsIPv6();
+    void testGetIPConnectionProtocol_hostIPv4_packetIPv6_returnsPacketValue();
+    void testGetIPConnectionProtocol_hostIPv6_packetIPv4_returnsPacketValue();
+    void testGetIPConnectionProtocol_hostMappedIPv4_returnsIPv4();
+    void testGetIPConnectionProtocol_hostEmpty_packetIPv4_returnsIPv4();
+    void testGetIPConnectionProtocol_hostEmpty_packetIPv6_returnsIPv6();
+    void testGetIPConnectionProtocol_bothEmpty_returnsIPv4();
+    void testGetIPConnectionProtocol_hostInvalid_packetIPv4_returnsIPv4();
 };
 
 
