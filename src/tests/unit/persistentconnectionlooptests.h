0094d1bb75152592eaa3cff925fc445fceb20277
Tomas Gallucci <professortom@gmail.com>
2026-04-11 14:53:12 -0500

    add persistentConnectionLoopTests class to projext

---
+++ src/tests/unit/persistentconnectionlooptests.h
@@ -0,0 +1,14 @@
+//
+// Created by Tomas Gallucci on 4/11/26.
+//
+
+#ifndef PERSISTENTCONNECTIONLOOPTESTS_H
+#define PERSISTENTCONNECTIONLOOPTESTS_H
+
+
+class PersistentConnectionLoopTests
+{
+};
+
+
+#endif //PERSISTENTCONNECTIONLOOPTESTS_H
