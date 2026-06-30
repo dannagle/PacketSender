e4b32eba36ac76fbdce6232e89fc4fea9d907423
Tomas Gallucci <professortom@gmail.com>
2026-06-30 11:19:07 -0500

    remove debugging code

--- src/connections/outgoingtcpconnection.cpp
+++ src/connections/outgoingtcpconnection.cpp
@@ -11,21 +11,15 @@ OutgoingTcpConnection::OutgoingTcpConnection(QObject* parent)
 
 void OutgoingTcpConnection::send(const Packet& packet)
 {
-    qDebug() << "OutgoingTcpConnection::send() thread_ ? true: false: " << (thread_ ? true : false);
-    // qDebug() << "OutgoingTcpConnection::send() thread_->metaObject()->className(): " << thread_->metaObject()->className();
-    qDebug() << "OutgoingTcpConnection::send() thread_.get(): " << thread_.get();
-    qDebug() << "OutgoingTcpConnection::send() thread_.currentThreadId(): " << thread_->currentThreadId();
     // Clean up any previous thread (one-shot finished, or we want a fresh one)
     if (thread_)
     {
-        qDebug() << "worst debug message ever: got inside if";
         thread_->shutdown();
         thread_.reset();
     }
 
     try
     {
-        qDebug() << "OutgoingTcpConnection::send() got inside the try";
         // Create fresh thread for this send
         thread_ = makeOutgoingTcpThread(packet);
 
