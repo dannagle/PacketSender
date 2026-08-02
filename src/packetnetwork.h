0ad2be84fea30698d60f67b14c1be0c1d82a3d8f
Tomas Gallucci <professortom@gmail.com>
2026-08-02 12:59:52 -0500

    Refactor multicast membership to track address + interface
    Replace QStringList with QList<MulticastMembership> so each joined
    group remembers the interface it was joined on. Update join/leave/
    find/canSend/multicastStringList accordingly, add selective leave
    overloads, and make the relevant methods const.

--- src/packetnetwork.h
+++ src/packetnetwork.h
@@ -73,7 +73,7 @@ class PacketNetwork : public QObject
         QList<int> getTCPPortsBound();
         QList<int> getSSLPortsBound();
 
-        QStringList multicastStringList();
+        QStringList multicastStringList() const;
 
         bool consoleMode;
 
@@ -109,10 +109,13 @@ class PacketNetwork : public QObject
 
         static bool isMulticast(QString ip);
 
-        void joinMulticast(QString address);
-        bool canSendMulticast(QString address);
+        bool joinMulticast(const QString& address, const QNetworkInterface &iface);
+        [[nodiscard]] bool canSendMulticast(const QString &address) const;
         void leaveMulticast();
-        QUdpSocket * findMulticast(QString multicast);
+        bool leaveMulticast(const QString &address, const QNetworkInterface &iface); // leave single multicast group on a given interface
+        bool leaveMulticast(const QString &address, const QString &ifaceName);
+
+        QUdpSocket * findMulticast(const QString &multicast) const;
         static bool DTLSisSupported();
         QList<DtlsServer *> dtlsServers;
 
@@ -142,7 +145,7 @@ private slots:
 private:
         //mapping of joined multicast groups
         //format is 239.255.120.19:5009, 239.255.120.23:5009
-        QStringList joinedMulticast;
+        QList<MulticastMembership> joinedMulticast;   // group address → interface
 
         QList<ThreadedTCPServer *> allTCPServers();
 
